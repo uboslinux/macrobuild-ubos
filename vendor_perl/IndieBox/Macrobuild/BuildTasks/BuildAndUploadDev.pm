5ded4ac8dcd09ca644644f3dc24ae95be29d02d3
Johannes Ernst <jernst@indiecomputing.com>
2014-07-22 18:16:06 +0000

    Different task structure for top-level reporting

--- vendor_perl/IndieBox/Macrobuild/BuildTasks/BuildAndUploadDev.pm
+++ vendor_perl/IndieBox/Macrobuild/BuildTasks/BuildAndUploadDev.pm
@@ -45,38 +45,43 @@ sub new {
                 'upconfigs'   => $repoUpConfigs->{$_},
                 'usconfigs'   => $repoUsConfigs->{$_},
                 'repository'  => $_ ) } @repos;
-        
     my @buildTaskNames = keys %$buildTasks;
-    my @mergeKeys      = ( '', @buildTaskNames );
     
-    $self->{delegate} = new Macrobuild::CompositeTasks::SplitJoin( 
-        'parallelTasks' => $buildTasks,
-        'joinTask'      => new Macrobuild::CompositeTasks::Sequential(
-            'tasks' => [
-                new IndieBox::Macrobuild::BasicTasks::Upload(
-                    'from'        => '${repodir}/${arch}',
-                    'to'          => 'buildmaster@depot.indiebox.net:/var/lib/cldstr-archdepot/a00000000000000000000000000000003/${arch}' ),
-                new Macrobuild::CompositeTasks::MergeValuesTask(
-                    'name'         => 'Merge update lists from dev repositories: ' . join( ' ', @repos ),
-                    'keys'         => \@mergeKeys ),
-                new Macrobuild::CompositeTasks::SplitJoin(
-                    'parallelTasks' => {
-                        'report-stdout' => new Macrobuild::BasicTasks::Report(
-                            'name'        => 'Report build activity for dev repositories: ' . join( ' ', @repos ),
-                            'fields'      => [ 'updated-packages', 'bootimages', 'vmdkimages', 'repository-synced-to' ] ),
-                        'report-mqtt' => new Macrobuild::BasicTasks::ReportViaMosquitto(
-                            'fieldsChannels' => {
-                                ''                     => '${mqttProducerId}/build/run',
-                                'updated-packages'     => '${mqttProducerId}/build/updated-packages',
-                                'bootimages'           => '${mqttProducerId}/build/bootimages',
-                                'vmdkimages'           => '${mqttProducerId}/build/vmdkimages',
-                                'repository-synced-to' => '${mqttProducerId}/build/repository-synced-to'
-                            }
-                        )
-                    }
+    my $uploadTasks = {};
+    map { $uploadTasks->{"upload-$_"} = new IndieBox::Macrobuild::BasicTasks::Upload(
+                'from'        => '${repodir}/${arch}/'    . $_,
+                'to'          => '${uploadDest}/${arch}/' . $_ ) } @repos;
+    my @uploadTaskNames = keys %$uploadTasks;
+            
+    my @mergeKeys = ( '', @buildTaskNames, @uploadTaskNames );
+    
+    $self->{delegate} = new Macrobuild::CompositeTasks::Sequential( 
+        'tasks' => [
+            new Macrobuild::CompositeTasks::SplitJoin( 
+                'parallelTasks' => $buildTasks,
+                'joinTask'      => new Macrobuild::CompositeTasks::Sequential(
+                    'tasks' => [
+                        new Macrobuild::CompositeTasks::SplitJoin(
+                            'parallelTasks' => $uploadTasks ),
+                        new Macrobuild::CompositeTasks::MergeValuesTask(
+                            'name'         => 'Merge update lists from dev repositories: ' . join( ' ', @repos ),
+                            'keys'         => \@mergeKeys )
+                    ]
                 )
-            ]
-        )
+            ),
+            new Macrobuild::BasicTasks::Report(
+                'name'        => 'Report build activity for dev repositories: ' . join( ' ', @repos ),
+                'fields'      => [ 'updated-packages', 'bootimages', 'vmdkimages', 'uploaded-to' ] ),
+            new Macrobuild::BasicTasks::ReportViaMosquitto(
+                'fieldsChannels' => {
+                    ''                 => '${mqttProducerId}/build/run',
+                    'updated-packages' => '${mqttProducerId}/build/updated-packages',
+                    'bootimages'       => '${mqttProducerId}/build/bootimages',
+                    'vmdkimages'       => '${mqttProducerId}/build/vmdkimages',
+                    'uploaded-to'      => '${mqttProducerId}/build/repository-synced-to'
+                }
+            )
+        ]
     );
     return $self;
 }
